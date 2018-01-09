098fbd74e1db0b17d3e58ee5f87f0f7324898ae5
YaoTe Tsai <yaote.tsai@senao.com.tw>
2018-01-09 12:05:54 +0800

    Init Project

---
+++ src/design_pattern/Clock.java
@@ -0,0 +1,17 @@
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Module Name          : design_pattern.Clock
+   Module Description   :
+
+   Date Created      : 2018¦~1¤ë9¤é
+   Original Author   : 015336
+   ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+package design_pattern;
+
+/**
+ * @author 015336
+ *
+ */
+public class Clock
+{
+
+}
---
+++ src/design_pattern/DigitalClock.java
@@ -0,0 +1,19 @@
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Module Name          : design_pattern.DigitalClock
+   Module Description   :
+
+   Date Created      : 2018¦~1¤ë9¤é
+   Original Author   : 015336
+   ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+package design_pattern;
+
+/**
+ * @author 015336
+ */
+public class DigitalClock
+{
+	public void update(int hours, int minutes, int seconds)
+	{
+
+	}
+}
